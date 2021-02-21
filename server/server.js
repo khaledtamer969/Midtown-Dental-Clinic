9584ec815ee61e258f513c61897e6570db00be91
khaledtamer969 <76225235+khaledtamer969@users.noreply.github.com>
2021-02-21 21:18:21 +0200

    development mode

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 const buildPath= path.join(__dirname, "..", "build");
 app.use(express.static(buildPath));
 app.get('*', (req, res) => {
-   res.sendFile(path.join(buildPath, 'index.html'));
+  res.sendFile(path.join(buildPath, 'index.html'));
 });
 app.listen(port, () => {
    console.log('Server is up!');
