206ad4e99a2bce59541c22c1bb53aeb5e3e3ec86
khaledtamer969 <76225235+khaledtamer969@users.noreply.github.com>
2021-02-21 15:49:27 +0200

    message fix server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(express.static(__dirname,'build'));
+app.use(express.static(path.join(__dirname,'build')));
 app.get('*', (req, res) => {
    res.sendFile(path.join(__dirname,'build', 'index.html'));
 });
